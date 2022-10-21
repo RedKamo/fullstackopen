e83e2c297cf5f30ac865461d7c79ed9b40cbdfc9
Camilo Suarez <camiloandressuarez1@gmail.com>
2022-10-21 13:09:17 -0500

    add backend phoneBook exercises // 3.15 - 3.18 / phoneBookBackend

--- part03/phoneBookBackend/index.js
+++ part03/phoneBookBackend/index.js
@@ -1,13 +1,23 @@
-//const http = require("http");
+require("dotenv").config();
+
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
-//const logger = morgan("combined");
 const cors = require("cors");
+const Person = require("./models/Person");
+const app = express();
 
-const PORT = process.env.PORT || 3001;
+//const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
-let persons = [
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(express.static("dist"));
+app.use(cors());
+
+app.use(morgan(":method :url :body"));
+app.use(express.json());
+
+/* let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -34,64 +44,94 @@ let persons = [
     phone: "555555-1233422",
   },
 ];
+ */
 
-//IMPLEMENTING MIDDLEWARE WITH MORGAN
-
-morgan.token("body", (req) => JSON.stringify(req.body));
-
-app.use(express.static("dist"));
-app.use(cors());
-
-app.use(morgan(":method :url :body"));
-app.use(express.json());
-//app.use(logger);
-
-app.get("/info", (request, response) => {
-  const requestTime = new Date(Date.now());
-  response.send(
-    `<h4>PhoneBook has info for ${persons.length} people </h4> <p>${requestTime}</p>`
-  );
-});
-
-//ADD NEW PERSON
-
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
-
-  if (!body.name || !body.phone) {
-    return res.status(400).json({
-      error: "name or phone is missing",
-    });
-  }
-  const person = {
-    name: body.name,
-    phone: body.phone,
-    id: Math.floor(Math.random() * 1000),
-  };
-
-  persons = persons.concat(person);
-
-  res.json(person);
+app.get("/api/persons", (req, res) => {
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 //GET ONE PERSON ONLY
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-  person ? res.json(person) : res.status(404).end();
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.get("/info", (req, res) => {
+  const requestTime = new Date(Date.now());
+  Person.find({}).then((persons) => {
+    res.send(
+      `<h4>PhoneBook has info for ${persons.length} people </h4> <p>${requestTime}</p>`
+    );
+  });
+});
+
+//ADD NEW PERSON
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  const contact = new Person({
+    name: body.name,
+    phone: body.phone,
+  });
+
+  contact
+    .save()
+    .then((savedContact) => savedContact.toJSON())
+    .then((saveAndFormattedContact) => {
+      res.json(saveAndFormattedContact);
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = request.body;
+
+  const contact = {
+    name: body.name,
+    phone: body.phone,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, contact, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 //CREATE ROUTE FOR DELETE ONE PERSON
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findOneAndDelete({ id: req.params.id })
+    .then((res) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
-});
+const unknowPort = (req, res) => {
+  res.status(404).send({ error: "unknow Port" });
+};
+
+app.use(unknowPort);
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(404).sedn({ error: "bad id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server runing on port ${PORT} `);
