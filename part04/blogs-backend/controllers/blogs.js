65b0488f42498a675c516e4087b917c64e8f0f4c
Camilo Suarez <camiloandressuarez1@gmail.com>
2022-10-24 12:19:23 -0500

    add exercises part04 -- 4.1 - 4.2 / blogs-backend

---
+++ part04/blogs-backend/controllers/blogs.js
@@ -0,0 +1,59 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get("/", (request, response)=> {
+    Blog.find({})
+    .then(blogs => {
+        response.json(blogs)
+    })
+})
+
+blogsRouter.get("/:id", (request, response, next)=> {
+    Blog.findById(request.params.id)
+    .then(blog=> {
+        blog ? response.json(blog) : response.status(404).end()
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.post("/", (request, response, next)=> {
+    const body = request.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0,
+    })
+
+    blog
+    .save()
+    .then((savedBlog) => savedBlog.toJSON())
+    .then((savedAndFormattedBlog) => response.json(savedAndFormattedBlog))
+    .catch((error) => next(error))
+})
+
+/* blogsRouter.delete('/:id', (request, response, next)=>{
+    Blog.findByIdAndRemove(request.params.id)
+    .then(()=>{
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (request, response, next)=>{
+    const body = request.body
+
+    const blog = {
+        content: body.content,
+        important: body.important
+    }
+
+    Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    .then(updateBlog=> {
+        response.json(updateBlog)
+    })
+    .catch(error => next(error))
+}) */
+
+module.exports = blogsRouter
