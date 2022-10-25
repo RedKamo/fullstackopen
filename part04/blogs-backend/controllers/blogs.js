b7bbc06e4504b9eccf738a006bae5e8b79b729b7
Camilo Suarez <camiloandressuarez1@gmail.com>
2022-10-24 19:06:11 -0500

    add exercises part04 -- 4.13 - 4.14 / blogs-backend

--- part04/blogs-backend/controllers/blogs.js
+++ part04/blogs-backend/controllers/blogs.js
@@ -36,6 +36,58 @@ blogsRouter.post('/', async(request, response)=> {
     }
 })
 
+/* blogsRouter.delete('/:id', (request, response, next)=>{
+    Blog.findByIdAndRemove(request.params.id)
+    .then(()=>{
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+}) */
+
+//async function delete one blog REFACTOR
+blogsRouter.delete('/:id', async(request,response)=> {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+/*
+blogsRouter.put('/:id', (request, response, next)=>{
+    const body = request.body
+
+     const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    .then(updateBlog=> {
+        response.json(updateBlog)
+    })
+    .catch(error => next(error))
+}) */
+
+//async function update Blog REFACTOR
+blogsRouter.put('/:id',async(request, response, next)=>{
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    const update = await Blog.findByIdAndUpdate(
+        request.params.id,
+        blog,
+        {new: true}
+    )
+    response.json(update.toJSON())
+})
+
+
 /* blogsRouter.post("/", (request, response, next)=> {
     const body = request.body
 
@@ -53,27 +105,7 @@ blogsRouter.post('/', async(request, response)=> {
     .catch((error) => next(error))
 }) */
 
-/* blogsRouter.delete('/:id', (request, response, next)=>{
-    Blog.findByIdAndRemove(request.params.id)
-    .then(()=>{
-        response.status(204).end()
-    })
-    .catch(error => next(error))
-})
 
-blogsRouter.put('/:id', (request, response, next)=>{
-    const body = request.body
 
-    const blog = {
-        content: body.content,
-        important: body.important
-    }
-
-    Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
-    .then(updateBlog=> {
-        response.json(updateBlog)
-    })
-    .catch(error => next(error))
-}) */
 
 module.exports = blogsRouter
